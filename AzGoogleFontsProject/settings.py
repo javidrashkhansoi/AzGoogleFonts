6b9697c64126bb0e16a73ba58c99c73595d8b64c
Javid <cavidgulmaliyev491995@gmail.com>
2021-11-10 01:24:52 +0400

    Modified settings.py

--- AzGoogleFontsProject/settings.py
+++ AzGoogleFontsProject/settings.py
@@ -51,12 +51,8 @@ WSGI_APPLICATION = 'AzGoogleFontsProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'googlefontsdb',
-        'USER': 'postgres',
-        'PASSWORD': 'Ma_Ce_Ra1123581321345589',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
